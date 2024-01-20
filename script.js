e3d5f02c059568bf6383180789979b234b8d577d
yashas k n <yashasknyashaskn229@gmail.com>
2024-01-20 19:41:44 +0530

    raghav

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ let arr = [
     console.log('Consoling Array Variable', arr);
     // Over here I can directly access the variable.
   }
-  let arr = [
+  let a = [
     { id: 1, name: 'john', age: 18, profession: 'developer' },
     { id: 2, name: 'jack', age: 20, profession: 'developer' },
     { id: 3, name: 'karen', age: 19, profession: 'admin' },
